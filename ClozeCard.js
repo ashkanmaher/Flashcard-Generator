83c8af3b9ff46a0442993920f6c814f58cc3983f
Ash Maher <ashkanmaherbus@gmail.com>
2017-09-03 15:17:55 -0700

    added function Clozecard that accepts two arguments text and cloze

---
+++ ClozeCard.js
@@ -0,0 +1,16 @@
+
+// Constructor function for creating cloze-deletion flashcards
+
+function ClozeCard(text, cloze) {
+this.cloze = cloze,
+this.partial =
+this.fullText = text,
+
+
+
+}
+
+
+
+
+module.exports = ClozeCard;
